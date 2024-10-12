a7197919367bf37f09c6c5ee20cf68efb01adfd9
p312734 <34969190+p312734@users.noreply.github.com>
2024-10-12 16:11:50 +0200

    2nd review

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/drp.db')
+engine = create_engine('sqlite:///./data/drp.db')
 df = pd.read_sql_table('msgcat', engine)
 
 # load model
-model = joblib.load("../models/drp_classifier.pkl")
+model = joblib.load("./models/drp_classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
